8c7b38c5acecdb753cb2de36a930325191938c83
Mayank <91525599+mayankjaiswall@users.noreply.github.com>
2026-09-10 07:36:01 +0530

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -9,6 +10,12 @@ class HomeController extends Controller
 
     public function index()
     {
-        return view('index');
+        $categories = Category::where('is_active', true)
+            ->orderBy('category_name')
+            ->get();
+
+        return view('index', [
+            'categories' => $categories,
+        ]);
     }
 }
